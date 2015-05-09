5c038fa419395803c078a022e929e069da7c83bd
Ellen Koenig <ellen.koenig@soundcloud.com>
2015-05-10 00:38:34 +0200

    Some fixes to plot 2

--- plot2.R
+++ plot2.R
@@ -1,8 +1,5 @@
-# This file constructs a barplot from data on "Global Active Power"
+# This file constructs a time series plot from data on "Global Active Power"
 # It assumes a file called household_power_consumption.txt with the data in the source folder
-
-# Imports
-library("plyr")
 
 # 1. Read the data
 data <- read.csv("household_power_consumption.txt", sep = ";", colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), na.strings = c('?'))
@@ -14,6 +11,6 @@ gap_time_series <- ts(relevant_data$Global_active_power, frequency = 1440)
 
 # 3. Plot the results and output to png file
 png("plot2.png")
-plot(gap_time_series, main = "Global Active Power", ylab = "Global Active Power (kilowatts)", xaxt = 'n')
+plot(gap_time_series, ylab = "Global Active Power (kilowatts)", xaxt = 'n')
 axis(1, at = c(1,2,3), labels = c("Thu", "Fri", "Sat"))
 dev.off()
